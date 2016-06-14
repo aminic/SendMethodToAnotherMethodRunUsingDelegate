8cb29e243ff606d0e1dc3bf0cb239039c55eb883
aminic <aminic.null@gmail.com>
2016-06-14 22:54:01 +0800

    SendMethodToAnotherMethodRunUsingDelegate

---
+++ SendMethodToAnotherMethodRunUsingDelegate/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendMethodToAnotherMethodRunUsingDelegate
+{
+
+    public class MessagePool
+    {
+        public string GetMessage()
+        {
+            return "this is message";
+        }
+    }
+
+    public class MainCaller
+    {
+
+
+
+        public void InvokeMethod(Action method)
+        {
+            method.Invoke();
+        }
+        public T InvokeMethod<T>(Func<T> method) where T : class
+        {
+            return method.Invoke() as T;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var w = new MessagePool();
+            var h = new MainCaller();
+
+            var result = h.InvokeMethod<string>(() => w.GetMessage());
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
